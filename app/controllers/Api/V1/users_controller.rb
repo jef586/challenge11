cd26c2e0da1c733f103140600163313f273614ab
Ezequiel Flores <jef_artista@hotmail.com>
2022-01-17 20:15:51 -0300

    primer commit

---
+++ app/controllers/Api/V1/users_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        
+      end
+      def create
+        
+      end
+      def update
+        
+      end
+      def show
+        
+      end
+      def delete
+        
+      end
+        
+    end
+  end
+end
